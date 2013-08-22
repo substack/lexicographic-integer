2ffe7561ae5327380d6d1698bbb3f5c7e2783697
James Halliday <mail@substack.net>
2013-08-22 15:57:19 -0700

    more precision output

--- test/precision.js
+++ test/precision.js
@@ -2,12 +2,14 @@ var convert = require('../');
 var test = require('tape');
 
 test('preserve precision', function (t) {
-    t.plan(2);
+    t.plan(3);
     
     var a = 1.000000000000001e100;
     var b = 1e100;
     var ha = convert(a, 'hex');
     var hb = convert(b, 'hex');
+    console.log(ha, '>', hb);
     t.notEqual(a, b);
     t.notEqual(ha, hb);
+    t.ok(ha > hb);
 });
