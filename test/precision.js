86239d915f492a85d7194d4018a4b17779c79098
James Halliday <mail@substack.net>
2013-08-22 15:56:19 -0700

    precision test fails in the right place

--- test/precision.js
+++ test/precision.js
@@ -2,9 +2,9 @@ var convert = require('../');
 var test = require('tape');
 
 test('preserve precision', function (t) {
-    t.plan(1);
+    t.plan(2);
     
-    var a = 1.0000000000000001e100;
+    var a = 1.000000000000001e100;
     var b = 1e100;
     var ha = convert(a, 'hex');
     var hb = convert(b, 'hex');
