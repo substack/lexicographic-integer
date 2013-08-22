d85acb48db1e33c65916bf334b898f08edbb76fe
James Halliday <mail@substack.net>
2013-08-22 15:25:08 -0700

    fix the >4 billion base case

--- x.js
+++ x.js
@@ -1,41 +1,14 @@
-var xs = [];
+var convert = require('./');
 
 var prev = convert(0);
-for (var n = 1; true; n++) {
-    var cur = convert(n);
+var skip = 1;
+for (var n = 1; n < Number.MAX_VALUE; n += skip) {
+    var cur = convert(n, 'hex');
     if (cur <= prev) break;
     prev = cur;
+    console.log(n, cur);
+    if (n > 256*256) {
+        skip = 1 + Math.pow(245, Math.ceil(Math.log(n) / Math.log(256)));
+    }
 }
 console.log(n - 1);
-
-function convert (n) {
-    var bytes;
-    var x = n - 252;
-    if (n < 252) {
-        bytes = [ n ];
-    }
-    else if (x < 256) {
-        bytes = [ 252, x ];
-    }
-    else if (x < 256*256) {
-        bytes = [ 253, Math.floor(x / 256), x % 256 ];
-    }
-    else if (x < 256*256*256) {
-        bytes = [
-            254,
-            Math.floor(x / 256 / 256),
-            Math.floor(x / 256) % 256,
-            x % 256
-        ];
-    }
-    else if (x < 256*256*256*256) {
-        bytes = [
-            255,
-            Math.floor(x / 256 / 256 / 256),
-            Math.floor(x / 256 / 256) % 256,
-            Math.floor(x / 256) % 256,
-            x % 256
-        ];
-    }
-    return Buffer(bytes).toString('hex');
-}
