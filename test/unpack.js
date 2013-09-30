47cdf31d7d52b7093b56092978bb4055f704ae51
James Halliday <mail@substack.net>
2013-09-30 02:49:37 +0100

    tweaked the test, now everything is passing

--- test/unpack.js
+++ test/unpack.js
@@ -11,12 +11,15 @@ test('unpack', function (t) {
     t.equal(n, Infinity);
     t.end();
     
-    function compare () {
-        t.equal(round(n), round(convert.unpack(cur)));
-    }
-    
-    function round (n) {
-        var x = Math.log(n) / Math.log(10);
-        return Math.round(n * n / Math.pow(10, x - 8));
+    function compare (a, b) {
+        var desc = a + ' !=~ ' + b
+        if (/e\+\d+$/.test(a) || /e\+\d+$/.test(b)) {
+            t.equal(String(a).slice(0,8), String(b).slice(0,8), desc);
+            t.equal(/e\+(\d+)$/.exec(a)[1], /e\+(\d+)$/.exec(b)[1], desc);
+        }
+        else {
+            t.equal(String(a).slice(0,8), String(b).slice(0,8), desc);
+            t.equal(String(a).length, String(b).length, desc);
+        }
     }
 });
